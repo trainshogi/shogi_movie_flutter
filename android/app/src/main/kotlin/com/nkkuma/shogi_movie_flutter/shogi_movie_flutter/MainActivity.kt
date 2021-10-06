68701ff269871bae9d6dfce609ca6d1e08d0b001
trainshogi <densyashogi@gmail.com>
2021-10-06 21:49:37 +0900

    detect piecePlace, detect one Space Piece and initial Place Detect(on going)

--- android/app/src/main/kotlin/com/nkkuma/shogi_movie_flutter/shogi_movie_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/nkkuma/shogi_movie_flutter/shogi_movie_flutter/MainActivity.kt
@@ -28,6 +28,7 @@ import java.lang.StringBuilder
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.coroutineContext
 import kotlin.coroutines.suspendCoroutine
+import kotlin.streams.toList
 
 
 class MainActivity: FlutterActivity() {
@@ -54,6 +55,19 @@ class MainActivity: FlutterActivity() {
 //        "P", "L", "N", "S", "G", "B", "R", "K",
 //        "+P", "+L", "+N", "+S", "+B", "+R"
     )
+
+    private val initialPiecePlacement = listOf(
+        listOf("vkyo", "vkei", "vgin", "vkin", "vou", "vkin", "vgin", "vkei", "vkyo"),
+        listOf("", "vhisya", "", "", "", "", "", "vkaku", ""),
+        listOf("vfu", "vfu", "vfu", "vfu", "vfu", "vfu", "vfu", "vfu", "vfu"),
+        listOf("", "", "", "", "", "", "", "", ""),
+        listOf("", "", "", "", "", "", "", "", ""),
+        listOf("", "", "", "", "", "", "", "", ""),
+        listOf("fu", "fu", "fu", "fu", "fu", "fu", "fu", "fu", "fu"),
+        listOf("", "kaku", "", "", "", "", "", "hisya", ""),
+        listOf("kyo", "kei", "gin", "kin", "gyoku", "kin", "gin", "kei", "kyo")
+    )
+
     private val pieceSizeList = listOf(64, 62, 60, 58, 56, 54, 52, 50, 48, 47, 46, 45, 44, 43, 42, 41, 40, 37, 35)
 //    private val pieceSizeList = listOf(46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35)
 //    private val pieceRotateList = listOf(20, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -15, -20)
@@ -110,7 +124,8 @@ class MainActivity: FlutterActivity() {
                     piecesSize = listOf(37, 42, 43, 47, 47, 48, 50, 50, 50, 37, 42, 43, 47, 47, 48, 50, 50, 50)
                 )
                 result.success(resultJson)
-            } else if (call.method == "piece_place_detect") {
+            }
+            else if (call.method == "piece_place_detect") {
 
                 // load opencv
                 if (!OpenCVLoader.initDebug())
@@ -134,6 +149,75 @@ class MainActivity: FlutterActivity() {
                     relativePoints = pointsFloatList
                 )
                 result.success(resultJson)
+            }
+            else if (call.method == "one_piece_detect") {
+
+                // load opencv
+                if (!OpenCVLoader.initDebug())
+                    Log.e("OpenCV", "Unable to load OpenCV!")
+                else
+                    Log.d("OpenCV", "OpenCV loaded Successfully!")
+
+                // get variable
+                val srcPath = call.argument<String>("srcPath").toString()
+                val dirName = call.argument<String>("dirName").toString()
+                val points = call.argument<String>("points").toString()
+                val space = call.argument<String>("space").toString()
+                Log.d("OpenCV", srcPath)
+                Log.d("OpenCV", dirName)
+                Log.d("OpenCV", points)
+                Log.d("OpenCV", space)
+
+                // example: perspective covert
+                val relativePoints = util.offsetString2FloatList(points)
+                val spaceList = space.split(',').map { it.toInt() }
+                Log.d("OpenCV", relativePoints.toString())
+                val targetPlaceMat = spaceCroppedMat(srcPath, relativePoints, spaceList)
+                val resultJson = detectPiece(
+                    dirName = dirName,
+                    pieceNameList = listOf(),
+                    piecesSize = listOf(),
+                    targetPlaceMat = targetPlaceMat
+//                    piecesSize = listOf(37, 42, 43, 47, 47, 48, 50, 50, 50, 37, 42, 43, 47, 47, 48, 50, 50, 50)
+                )
+                result.success(resultJson)
+            }
+            else if (call.method == "initial_piece_detect") {
+
+                // load opencv
+                if (!OpenCVLoader.initDebug())
+                    Log.e("OpenCV", "Unable to load OpenCV!")
+                else
+                    Log.d("OpenCV", "OpenCV loaded Successfully!")
+
+                // get variable
+                val srcPath = call.argument<String>("srcPath").toString()
+                val dirName = call.argument<String>("dirName").toString()
+                val points = call.argument<String>("points").toString()
+                Log.d("OpenCV", srcPath)
+                Log.d("OpenCV", dirName)
+                Log.d("OpenCV", points)
+
+
+                val relativePoints = util.offsetString2FloatList(points)
+                val positionJson = getCurrentPosition(
+                    srcpath = srcPath,
+                    relativePoints = relativePoints
+                )
+
+                // TODO: parse json and detect Pieces and create Json
+//                // example: perspective covert
+//                val spaceList = space.split(',').map { it.toInt() }
+//                Log.d("OpenCV", relativePoints.toString())
+//                val targetPlaceMat = spaceCroppedMat(srcPath, relativePoints, spaceList)
+//                val resultJson = detectPiece(
+//                    dirName = dirName,
+//                    pieceNameList = listOf(),
+//                    piecesSize = listOf(),
+//                    targetPlaceMat = targetPlaceMat
+////                    piecesSize = listOf(37, 42, 43, 47, 47, 48, 50, 50, 50, 37, 42, 43, 47, 47, 48, 50, 50, 50)
+//                )
+                result.success(positionJson)
             } else {
                 result.notImplemented()
             }
@@ -185,34 +269,57 @@ class MainActivity: FlutterActivity() {
         return matCropped
     }
 
-    private fun matchTemplateWithRotate(pieceRotate: Int, resizedPieceMat: Mat, resizedMaskMat: Mat, matCropped: Mat): Set<Point> {
+    private fun file2crop9x9Mat(srcpath: String, relativePoints: List<List<Float>>): Mat {
+        // Bitmapを読み込み
+        val img = fileController.readImageFromFileWithRotate(srcpath)
+        // BitmapをMatに変換する
+        val matSource = Mat()
+        Utils.bitmapToMat(img, matSource)
+        Log.d("OpenCV", matSource.size().toString())
+
+        // convert relativePoints to absolutePoints
+        val absolutePoints = util.relativePoints2absolutePoints(relativePoints, img!!.getWidth(), img!!.getHeight())
+        Log.d("OpenCV", absolutePoints.toString())
+
+        val matCropped = crop9x9Img(matSource, absolutePoints)
+        Imgproc.cvtColor(matCropped, matCropped, Imgproc.COLOR_BGR2GRAY)
+        matCropped.convertTo(matCropped, CvType.CV_8UC1)
+        return matCropped
+    }
+
+    private fun matchTemplateWithRotateAndDetectPlace(pieceRotate: Int, resizedPieceMat: Mat, resizedMaskMat: Mat, matCropped: Mat): Set<Point> {
         val foundSpaces = mutableSetOf<Point>()
-        // rotate piece image
-        val rotatedPieceMat = util.rotateMat(resizedPieceMat, pieceRotate.toDouble())
-        val rotatedMaskMat = util.rotateMat(resizedMaskMat, pieceRotate.toDouble())
-        // threshold to 127
         // matchTemplate > threshold(0.65)
-        val result = Mat()
-        Imgproc.matchTemplate(matCropped, rotatedPieceMat, result, Imgproc.TM_CCOEFF_NORMED, rotatedMaskMat)
+        val result = matchTemplateWithRotate(pieceRotate, resizedPieceMat, resizedMaskMat, matCropped)
         Imgproc.threshold(result, result, MATCH_THRESHOLD, 1.0, Imgproc.THRESH_TOZERO);
         // add place to foundlist
         for (i in 0 until result.rows()) {
             for (j in 0 until result.cols()) {
                 if (result[i, j][0] > 0) {
                     foundSpaces.add(Point(
-                        ((j + rotatedPieceMat.cols()/2)/SPACE_WIDTH).toDouble(),
-                        ((i + rotatedPieceMat.rows()/2)/SPACE_HEIGHT).toDouble()
+                        ((j + resizedPieceMat.cols()/2)/SPACE_WIDTH).toDouble(),
+                        ((i + resizedPieceMat.rows()/2)/SPACE_HEIGHT).toDouble()
                     ))
                     Imgproc.rectangle(
                         matCropped,
                         Point(j.toDouble(), i.toDouble()),
-                        Point((j + rotatedPieceMat.cols()).toDouble(), (i + rotatedPieceMat.rows()).toDouble()),
+                        Point((j + resizedPieceMat.cols()).toDouble(), (i + resizedPieceMat.rows()).toDouble()),
                         Scalar(0.0, 0.0, 255.0)
                     )
                 }
             }
         }
         return foundSpaces
+    }
+
+    private fun matchTemplateWithRotate(pieceRotate: Int, resizedPieceMat: Mat, resizedMaskMat: Mat, matCropped: Mat): Mat {
+        // rotate piece image
+        val rotatedPieceMat = util.rotateMat(resizedPieceMat, pieceRotate.toDouble())
+        val rotatedMaskMat = util.rotateMat(resizedMaskMat, pieceRotate.toDouble())
+        // matchTemplate
+        val result = Mat()
+        Imgproc.matchTemplate(matCropped, rotatedPieceMat, result, Imgproc.TM_CCOEFF_NORMED, rotatedMaskMat)
+        return result
     }
 
     // crop image and matchTemplate pieces
@@ -255,34 +362,9 @@ class MainActivity: FlutterActivity() {
         // for piece
         for ((index, pieceName) in pieceNameListEnglish.withIndex()) {
 //        pieceNameListEnglish.withIndex().toList().parallelStream().forEach { (index, pieceName) ->
-            // if piece name starts v, remove v and set second hand piece flag to true
-            val secondHandPiece = pieceName[0] == 'v'
-            // Bitmapを読み込み
-            val filePath = "${dirName}/${pieceName.removePrefix("v")}"
-            val pieceImg = fileController.readImageFromFileWithRotate("${filePath}.jpg")
-            // BitmapをMatに変換する
-            val pieceMat = Mat()
-            Utils.bitmapToMat(pieceImg, pieceMat)
-            Log.d("OpenCV", pieceMat.size().toString())
-            // create mask image by fillConvexPoly
-            val rowPointsString = File("${filePath}.txt").readText()
-            val pieceRelativePoints = util.offsetString2MatOfPoint(rowPointsString.split("/")[1])
-            val pieceAbstractPoints = util.relativeMatOfPoint2AbsoluteMatOfPoint(pieceRelativePoints, pieceImg!!.width, pieceImg.height)
-            val maskMat = Mat.zeros(pieceMat.size(), CvType.CV_8U)
-            Imgproc.fillConvexPoly(maskMat, pieceAbstractPoints, Scalar(255.0, 255.0, 255.0))
-            // crop image
-            var croppedPieceMat = util.cropImageByMatOfPoint(pieceMat, pieceAbstractPoints)
-            var croppedMaskMat = util.cropImageByMatOfPoint(maskMat, pieceAbstractPoints)
-            // binalize
-            Imgproc.cvtColor(croppedPieceMat, croppedPieceMat, Imgproc.COLOR_BGR2GRAY)
-//            Imgproc.cvtColor(croppedMaskMat, croppedMaskMat, Imgproc.COLOR_BGR2GRAY)
-//            croppedPieceMat = util.binalizeColorMat(croppedPieceMat)
-//            croppedMaskMat = util.binalizeColorMat(croppedMaskMat)
-            // if piece name starts v, reverse image
-            if (secondHandPiece) {
-                croppedPieceMat = util.rotateMat(croppedPieceMat,180.0)
-                croppedMaskMat = util.rotateMat(croppedMaskMat,180.0)
-            }
+            val croppedMatList = createCroppedMat(dirName, pieceName)
+            val croppedPieceMat = croppedMatList[0]
+            val croppedMaskMat = croppedMatList[1]
 
             // for piece size
             val pieceSizeListForPiece = if (piecesSize != null) listOf(piecesSize[index]) else pieceSizeList
@@ -304,7 +386,7 @@ class MainActivity: FlutterActivity() {
                 val foundSpaces = mutableSetOf<Point>()
                 // for piece rotate
                 pieceRotateList.parallelStream().forEach { pieceRotate ->
-                    foundSpaces.addAll(matchTemplateWithRotate(pieceRotate, resizedPieceMat, resizedMaskMat, matCropped))
+                    foundSpaces.addAll(matchTemplateWithRotateAndDetectPlace(pieceRotate, resizedPieceMat, resizedMaskMat, matCropped))
                 }
 
                 // apply to sfen
@@ -349,13 +431,12 @@ class MainActivity: FlutterActivity() {
         return circleMat
     }
 
-
     // crop image and matchTemplate pieces
-    @RequiresApi(VERSION_CODES.N)
+//    @RequiresApi(VERSION_CODES.N)
     private fun getCurrentPosition(
         srcpath: String,
         relativePoints: List<List<Float>>
-    ): String? {
+    ): String {
         // static variable for get piece position
         val pieceExistThreshold = SPACE_HEIGHT * SPACE_WIDTH * 0.01
         var sfen = "111111111/111111111/111111111/111111111/111111111/111111111/111111111/111111111/111111111"
@@ -413,5 +494,86 @@ class MainActivity: FlutterActivity() {
         Log.d("OpenCV", rootObject.toString())
 
         return rootObject.toString()
+    }
+
+    private fun spaceCroppedMat(srcpath: String, relativePoints: List<List<Float>>, spaceList: List<Int>): Mat {
+        val matCropped = file2crop9x9Mat(srcpath, relativePoints)
+        // crop target place
+        val roi = Rect(SPACE_WIDTH*spaceList[0], SPACE_HEIGHT*spaceList[1], SPACE_WIDTH, SPACE_HEIGHT)
+        return Mat(matCropped, roi)
+    }
+
+    // crop image and matchTemplate pieces
+    @RequiresApi(VERSION_CODES.N)
+    private fun detectPiece(
+        dirName: String,
+        pieceNameList: List<String>,
+        piecesSize: List<Int>,
+        targetPlaceMat: Mat
+    ): String {
+        // for piece
+        val pieceNameListForPiece = if (pieceNameList.isNotEmpty()) pieceNameList else pieceNameListEnglish
+//        for ((index, pieceName) in pieceNameListEnglish.withIndex()) {
+        val result = pieceNameListForPiece.asSequence().withIndex().toList().map { (index, pieceName) ->
+            val croppedMatList = createCroppedMat(dirName, pieceName)
+            val croppedPieceMat = croppedMatList[0]
+            val croppedMaskMat = croppedMatList[1]
+            val ratio = croppedPieceMat.height().toDouble() / croppedPieceMat.width().toDouble()
+
+            // for piece size
+            val pieceSizeListForPiece = if (piecesSize.isNotEmpty()) listOf(piecesSize[index]) else pieceSizeList
+
+            pieceSizeListForPiece.parallelStream().map { pieceSize ->
+                // resize piece image
+                val resizedPieceMat = Mat()
+                val resizedMaskMat = Mat()
+                Imgproc.resize(croppedPieceMat, resizedPieceMat, Size(pieceSize.toDouble(), pieceSize*ratio))
+                Imgproc.resize(croppedMaskMat, resizedMaskMat, Size(pieceSize.toDouble(), pieceSize*ratio))
+
+                // for piece rotate
+                pieceRotateList.parallelStream().map { pieceRotate ->
+                    Core.minMaxLoc(matchTemplateWithRotate(pieceRotate, resizedPieceMat, resizedMaskMat, targetPlaceMat)).maxVal
+                }.toList().maxOrNull() ?: 0.0
+            }.toList().maxOrNull() ?: 0.0
+            // end loop of piece
+        }.withIndex().toList().maxByOrNull { it.value }
+
+        val pieceName = if (result != null) pieceNameListEnglish[result.index] else ""
+
+        // create json
+        val rootObject = JSONObject()
+        rootObject.put("piece", pieceName)
+        Log.d("OpenCV", rootObject.toString())
+
+        return rootObject.toString()
+    }
+
+    private fun createCroppedMat(dirName: String, pieceName: String): List<Mat> {
+        // if piece name starts v, remove v and set second hand piece flag to true
+        val secondHandPiece = pieceName[0] == 'v'
+        // Bitmapを読み込み
+        val filePath = "${dirName}/${pieceName.removePrefix("v")}"
+        val pieceImg = fileController.readImageFromFileWithRotate("${filePath}.jpg")
+        // BitmapをMatに変換する
+        val pieceMat = Mat()
+        Utils.bitmapToMat(pieceImg, pieceMat)
+        Log.d("OpenCV", pieceMat.size().toString())
+        // create mask image by fillConvexPoly
+        val rowPointsString = File("${filePath}.txt").readText()
+        val pieceRelativePoints = util.offsetString2MatOfPoint(rowPointsString.split("/")[1])
+        val pieceAbstractPoints = util.relativeMatOfPoint2AbsoluteMatOfPoint(pieceRelativePoints, pieceImg!!.width, pieceImg.height)
+        val maskMat = Mat.zeros(pieceMat.size(), CvType.CV_8U)
+        Imgproc.fillConvexPoly(maskMat, pieceAbstractPoints, Scalar(255.0, 255.0, 255.0))
+        // crop image
+        var croppedPieceMat = util.cropImageByMatOfPoint(pieceMat, pieceAbstractPoints)
+        var croppedMaskMat = util.cropImageByMatOfPoint(maskMat, pieceAbstractPoints)
+        // binalize
+        Imgproc.cvtColor(croppedPieceMat, croppedPieceMat, Imgproc.COLOR_BGR2GRAY)
+        // if piece name starts v, reverse image
+        if (secondHandPiece) {
+            croppedPieceMat = util.rotateMat(croppedPieceMat,180.0)
+            croppedMaskMat = util.rotateMat(croppedMaskMat,180.0)
+        }
+        return listOf(croppedPieceMat, croppedMaskMat)
     }
 }
